3bfbe39dce571c7a20c66b26ef463ad2c8e6b2a0
Hexiang Qi <qihexiang@outlook.com>
2023-08-12 20:13:39 +0800

    add 404 page

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hackernews"
+  base: "/hackernews",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        "404": "./404.html"
+      }
+    }
+  }
 })
